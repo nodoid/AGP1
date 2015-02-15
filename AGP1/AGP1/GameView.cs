7629396441ef5240f105ad606efbf4f36e7beb2b
nodoid <paul@snuberry-software.co.uk>
2015-02-15 21:23:15 +0000

    Initial import

---
+++ AGP1/AGP1/GameView.cs
@@ -0,0 +1,24 @@
+﻿using Android.Views;
+using Android.Graphics;
+using Android.Content;
+
+namespace AGP1
+{
+    public class GameView : View
+    {
+        Bitmap bmp;
+
+        public GameView(Context context)
+            : base(context)
+        {
+            bmp = BitmapFactory.DecodeResource(Resources, Resource.Drawable.Icon);
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            canvas.DrawColor(Color.Argb(255, 69, 119, 102));
+            canvas.DrawBitmap(bmp, 50, 100, null);
+        }
+    }
+}
+
---
+++ AGP1/AGP1/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Views;
+using Android.OS;
+
+namespace AGP1
+{
+    [Activity(Label = "AGP1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            SetContentView(new GameView(this));
+        }
+    }
+}
+
+
